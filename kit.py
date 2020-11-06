96a0aa32aaf854d50e7510b560a8abfe2fab1ee3
randomniichan <leokolo@hotmail.fr>
2020-11-06 14:53:45 +0100

    adding kit class

---
+++ kit.py
@@ -0,0 +1,22 @@
+class Kit : 
+    """
+    number,
+    model,
+    series,
+    yenPrice,
+    releaseDate,
+    notes
+    """
+
+    def __init__(self, args, imageLink, grade):
+        self.grade = grade
+        self.number = args[0]
+        self.model = args[1]
+        self.series = args[2]
+        self.yenPrice = args[3]
+        self.releaseDate = args[4]
+        self.notes = args[5]
+        self.imageLink = imageLink
+
+    def __str__(self):
+        return f'{self.grade}-{self.number} : {self.model}, from {self.series}, released {self.releaseDate}, priced at {self.yenPrice} yen. {self.notes}'
--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 import re
-import json
+from kit import Kit
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
@@ -10,35 +10,41 @@ headless = Options()
 headless.headless = True
 driver = webdriver.Firefox(options=headless)
 
+# details that are fetched
 details = ["Number","Model","Series","Yen Price","Release Date","Notes"]
 
 driver.get("https://gundam.fandom.com/wiki/Real_Grade#Regulars_and_Special_Editions")
-
 table =  driver.find_element(By.CSS_SELECTOR,".tabber.tabberlive")
 years = table.find_elements(By.CSS_SELECTOR, "ul.tabbernav>li>a")
+# getting years as strings
 years = map(lambda l : l.text, years)
 
 for y in years : 
+    # fetching all kits for this year
     kits = table.find_elements(By.CSS_SELECTOR,f'div.tabbertab[title=\"{str(y)}\"] tr:nth-child(n+2)')
-    for k in kits : 
-        for d in details : 
-            index = details.index(d)
-            element = k.find_element(By.CSS_SELECTOR,"td:nth-child("+str(index+1)+")")
-            info = element.get_attribute('textContent').strip()
-            if index == 0 :
-                try : 
-                    info = re.search("^[0-9a-zA-Z-\/]+",info).group(0)
-                except : 
-                    info = "N/A"
-                # fetching image
-                try :
-                    imageLink = element.find_element(By.CSS_SELECTOR,"a.image").get_attribute("href").strip()
-                    print(f'Image Link: {imageLink}')
-                except NoSuchElementException :
-                    print("No Image provided")
+    for k in kits :
+        # WebElements contenant les valeurs
+        attributesAsElements = k.find_elements(By.CSS_SELECTOR,"td")
+        attributesAsElements = attributesAsElements[:len(attributesAsElements)-2] #removing the last 2 elements to prevent the MSJoints and included hands
 
-            print(d+" : "+info)
+        # Valeurs encodées en tant que strings
+        attributes = list(map(lambda element : element.get_attribute('textContent').strip(), attributesAsElements))
+        try:
+            attributes[0] = re.search("^[0-9a-zA-Z-\\/]+",attributes[0]).group(0)
+        except :
+            attributes[0] = "N/A"
 
-        print("-------")
+        attributes[3] = int(re.sub("[^0-9]+","", attributes[3]))
+
+        # fetching image
+        try :
+            imageLink = attributesAsElements[0].find_element(By.CSS_SELECTOR,"a.image").get_attribute("href").strip()
+        except NoSuchElementException :
+            imageLink = "No Image Provided"
+        
+        kit = Kit(attributes, imageLink, "RG")
+
+        print(kit)
+        print("--------")
 
 driver.quit()
