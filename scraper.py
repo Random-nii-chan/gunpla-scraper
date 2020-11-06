7cef517011244659acfe361963c54fdbb156d7db
randomniichan <leokolo@hotmail.fr>
2020-11-06 11:51:08 +0100

    fetching all real grades

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import re
+import json
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
@@ -15,24 +16,29 @@ driver.get("https://gundam.fandom.com/wiki/Real_Grade#Regulars_and_Special_Editi
 
 table =  driver.find_element(By.CSS_SELECTOR,".tabber.tabberlive")
 years = table.find_elements(By.CSS_SELECTOR, "ul.tabbernav>li>a")
+years = map(lambda l : l.text, years)
 
-kits = table.find_elements(By.CSS_SELECTOR,"div.tabbertab[title=\"2021\"] tr:nth-child(n+2)")
-for k in kits : 
-    for d in details : 
-        index = details.index(d)
-        element = k.find_element(By.CSS_SELECTOR,"td:nth-child("+str(index+1)+")")
-        info = element.get_attribute('textContent').strip()
-        if index == 0 :
-            info = re.search("^[0-9]+",info).group(0)
-            # fetching image
-            try :
-                imageLink = element.find_element(By.CSS_SELECTOR,"a.image").get_attribute("href").strip()
-                print(f'Image Link: {imageLink}')
-            except NoSuchElementException :
-                print("No Image provided")
+for y in years : 
+    kits = table.find_elements(By.CSS_SELECTOR,f'div.tabbertab[title=\"{str(y)}\"] tr:nth-child(n+2)')
+    for k in kits : 
+        for d in details : 
+            index = details.index(d)
+            element = k.find_element(By.CSS_SELECTOR,"td:nth-child("+str(index+1)+")")
+            info = element.get_attribute('textContent').strip()
+            if index == 0 :
+                try : 
+                    info = re.search("^[0-9a-zA-Z-\/]+",info).group(0)
+                except : 
+                    info = "N/A"
+                # fetching image
+                try :
+                    imageLink = element.find_element(By.CSS_SELECTOR,"a.image").get_attribute("href").strip()
+                    print(f'Image Link: {imageLink}')
+                except NoSuchElementException :
+                    print("No Image provided")
 
-        print(d+" : "+info)
+            print(d+" : "+info)
 
-    print("-------")
+        print("-------")
 
 driver.quit()
